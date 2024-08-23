d20fdb8c7c96cdbd87e950a8844c585e4c0eee79
parthiban <parthiban@longshore.io>
2024-08-23 18:52:29 +0530

    tech changed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  let arr = ["MUI", "Ant Design", "Ag Grid", "git", "github"];
+  const tech = useMemo(
+    () =>
+      isMobile
+        ? technologies.filter((item) => !arr.includes(item.name))
+        : technologies,
+    [isMobile]
+  );
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-27" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      {tech &&
+        tech.map((technology) => (
+          <div className="w-28 h-27" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
     </div>
   );
 };
